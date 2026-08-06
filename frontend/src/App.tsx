7761d182538f7c357ee21a1d3a8375f468a87c95
Farhaan <farhaabkhanff@gmail.com>
2026-08-06 22:48:33 +0530

    chore: scaffold React TypeScript frontend

---
+++ frontend/src/App.tsx
@@ -0,0 +1,14 @@
+function App() {
+  return (
+    <main>
+      <img
+        src="/blacklight_logo.png"
+        alt="Blacklight"
+        width={388}
+        height={90}
+      />
+    </main>
+  );
+}
+
+export default App;
